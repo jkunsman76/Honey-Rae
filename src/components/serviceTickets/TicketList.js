51424100642295e1fdf68ff26584a41bac8d2eac
jkuns76 <jkoonman@gmail.com>
2021-11-23 11:26:00 -0600

    ready to start explore chapters

--- src/components/serviceTickets/TicketList.js
+++ src/components/serviceTickets/TicketList.js
@@ -4,38 +4,43 @@ import { useHistory, Link } from "react-router-dom";
 export const TicketList = () => {
     const [tickets, updateTickets] = useState([])
     const history = useHistory()
+    const fetchTickets = () => {fetch("http://localhost:8088/serviceTickets?_expand=employee&_expand=customer")
+    .then(res => res.json()).then(
+        (ticketData) => {
+            updateTickets(ticketData)
+        }
+    )
+    }
+
     useEffect(
-        () => {
-            console.log("fetching data")
-            fetch("http://localhost:8088/serviceTickets?_expand=employee&_expand=customer")
-                .then(res => res.json())
-                .then(
-                    (ticketData) => {
-                        updateTickets(ticketData)
-                     }
-                )
+        () => { fetchTickets()
         },
         []
     )
-console.log(tickets)
+
+    const deleteTicket = (id) => {
+        fetch(`http://localhost:8088/serviceTickets/${id}`, {
+            method: "DELETE"
+        })
+        fetchTickets()
+    }
+
     return (
         <>
-        <div>
-        <button onClick={() => history.push("/tickets/create")}>Create Ticket</button>
-        </div>
-
-        {
-            tickets.map(
-                (ticket) => {
-                    
-                    return <p className= {`${ticket.emergency ? 'Emergency': 'Ticket'}`}> 
-                    {ticket.emergency ? "🚑" : ""} <Link to={`/tickets/${ticket.id}`}>{ticket.description}</Link>
- submitted by {ticket.customer.name} and worked on by {ticket.employee.name}
-                    </p>
-                
-                 }
-            )
-        }
+            <div>
+                <button onClick={() => history.push("/tickets/create")}>Create Ticket</button>
+            </div>
+            {
+                tickets.map(
+                    (ticket) => {
+                        return <p className={`${ticket.emergency ? 'Emergency' : 'Ticket'}`}>
+                            {ticket.emergency ? "🚑" : ""} <Link to={`/tickets/${ticket.id}`}>{ticket.description}</Link>
+                            submitted by {ticket.customer.name} and worked on by {ticket.employee.name}
+                            <button onClick={() => {deleteTicket(ticket.id)}}>Delete</button>
+                        </p>
+                    }
+                )
+            }
         </>
     )
 }
